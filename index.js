11c54005024b3cf6e2e5202d0f50ae3ecce37fa2
Zaid Abdulameer <zabdulam33r@gmail.com>
2023-12-31 14:59:42 -0500

    Add documentation for getSheetsData

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ function toObject(site) {
     }))
     return data;
 }
+/** Pulls google sheets data
+ * @param {Object} config 
+ * @param {string} config.fileId The file id
+ * @param {string} config.gid The sheet id
+ * @param {string} config.range The range of cells (eg. A1:B2)
+ * @returns {Promise<{table:[][]}>} Returns data from google sheet
+ */
 async function getSheetData(config) {
     let {fileId, gid, range} = config;
     let url = `https://docs.google.com/spreadsheets/d/${fileId}/htmlembed/sheet?gid=${gid}&range=${range}`
